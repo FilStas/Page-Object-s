d9a174f0c119ed8b5ea2e53e2183674aa6611234
FilStas <filstas23@gmail.com>
2020-04-15 18:23:16 +0400

    fix files

--- src/test/java/ru/Netology/TestingTransferFunctionFromCardToCard.java
+++ src/test/java/ru/Netology/TestingTransferFunctionFromCardToCard.java
@@ -21,10 +21,12 @@ public class TestingTransferFunctionFromCardToCard {
         $("[data-test-id=\"from\"] input") .setValue("5559 0000 0000 0002");
         $("[data-test-id=\"action-transfer\"]") .click();
         $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"] .button") .click();
+        $("[data-test-id='amount'] input").clear();
         $("[data-test-id='amount'] input") .setValue("1");
         $("[data-test-id=\"from\"] input") .setValue("5559 0000 0000 0001");
         $("[data-test-id=\"action-transfer\"]") .click();
         $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit'] .button__text") .click();
+        $("[data-test-id='amount'] input").clear();
         $("[data-test-id='amount'] input") .setValue("9999");
         $("[data-test-id=\"from\"] input") .setValue("5559 0000 0000 0002");
         $("[data-test-id=\"action-transfer\"]") .click();
