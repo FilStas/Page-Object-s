3c2f2eafcde185a2f1aa319a83cf9835466f735e
FilStas <filstas23@gmail.com>
2020-04-15 16:46:39 +0400

    fix files java

--- src/main/java/ru/Netology/DataHelper.java
+++ src/main/java/ru/Netology/DataHelper.java
@@ -26,6 +26,7 @@ public class DataHelper {
 
         return new VerificationCode("12345");
     }
+
 }
 
 
--- src/test/java/ru/Netology/TestingTransferFunctionFromCardToCard.java
+++ src/test/java/ru/Netology/TestingTransferFunctionFromCardToCard.java
@@ -3,6 +3,7 @@ package ru.Netology;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestingTransferFunctionFromCardToCard {
@@ -15,5 +16,21 @@ public class TestingTransferFunctionFromCardToCard {
         val verificationPage = loginPage.validLogin(authInfo);
         val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         verificationPage.validVerify(verificationCode);
+        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit'] .button__text") .click();
+        $("[data-test-id='amount'] input") .setValue("1");
+        $("[data-test-id=\"from\"] input") .setValue("5559 0000 0000 0002");
+        $("[data-test-id=\"action-transfer\"]") .click();
+        $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"] .button") .click();
+        $("[data-test-id='amount'] input") .setValue("1");
+        $("[data-test-id=\"from\"] input") .setValue("5559 0000 0000 0001");
+        $("[data-test-id=\"action-transfer\"]") .click();
+        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit'] .button__text") .click();
+        $("[data-test-id='amount'] input") .setValue("9999");
+        $("[data-test-id=\"from\"] input") .setValue("5559 0000 0000 0002");
+        $("[data-test-id=\"action-transfer\"]") .click();
+
+
     }
+
+
 }
